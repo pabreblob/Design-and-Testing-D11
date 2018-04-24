bc6f8a12c9e096674c2f15ab4022b8c59fca4df3
Daniel <danielcarpioc@gmail.com>
2018-04-24 15:40:29 +0200

    .

--- Acme-Newspaper/src/main/java/repositories/FolderRepository.java
+++ Acme-Newspaper/src/main/java/repositories/FolderRepository.java
@@ -2,6 +2,7 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Folder;
@@ -9,4 +10,6 @@ import domain.Folder;
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Integer> {
 
+	@Query("select f from Actor a join a.folders f where a.id = ?1 and f.name = ?2")
+	Folder findFolderByNameAndActor(Integer actorId, String s);
 }
